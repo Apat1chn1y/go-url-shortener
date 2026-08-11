ea26171c96ffeb1ec2675719f36587fbf14ef63d
Apat1chn1y <gordeevdimon@gmail.com>
2026-08-11 15:50:53 -0400

    добавлены переменные для информации о сборке

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -17,7 +18,24 @@ import (
 	"github.com/rs/zerolog"
 )
 
+// Информация о сборке, заполняется при линковке через -ldflags.
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
+// printBuildInfo выводит информацию о версии, дате и коммите сборки.
+func printBuildInfo() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+}
+
 func main() {
+	// Выводим информацию о сборке
+	printBuildInfo()
+
 	// Настройка логгера: вывод в stdout в формате JSON (без ConsoleWriter, чтобы избежать паники)
 	logger := zerolog.New(os.Stdout).Level(zerolog.InfoLevel).With().Timestamp().Logger()
 
